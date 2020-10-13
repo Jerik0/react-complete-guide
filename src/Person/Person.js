b22195ce13bbcc8423d889a6aafa883011d3d225
jerik0 <jcarrico1985@live.com>
2020-10-13 11:27:01 -0500

    add ability to read props.children (content inserted between opening and closing tags of component) in the Person component itself

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -3,11 +3,13 @@ import React from 'react';
 // In it's simplest form, a 'Component' is just a plain JavaScript function that returns some JSX. 
 // ***NOTE: THIS IS KNOWN AS A STATELESS, OR DUMB COMPONENT. YOU CANNOT USE STATE WITH THIS***
 const person = props => {
-
   return (
-    <p>I'm {props.name}, and I am {props.age} years old.</p>
+    <div>
+      <p>I'm {props.name}, and I am {props.age} years old.</p>
+      <p>{props.children}</p>
+    </div>
+  // props.children refers to ANYTHING between the opening and closing tag of the component (including complex JS).
   )
-
 }
 
 export default person; // <<< This needs to be lowercase because it matches the function name.
