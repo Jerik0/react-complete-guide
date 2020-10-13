7963f7301f11669518c5f5e080e62cf941c69232
jerik0 <jcarrico1985@live.com>
2020-10-13 10:33:20 -0500

    create Person component and insert into App component for rendering. Add additional comments about react functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person' // <<< This needs to be UPPERCASE even though the Person.js is exported with lowercase.
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <h1>Hi, I'm a React App.</h1>
+        <Person />
       </div>
+   // <div></div> <<< this is bad form, though loosely and technically allowed, we should only have one root element.
     );
 
     // React.createElement('div', {className: 'App'}, React.createElement('h1', null, "Hi, I'm a React App.")) <<< what the above JSX gets compiled to in JS. 
---
+++ src/Person/Person.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+// In it's simplest form, a 'Component' is just a plain JavaScript function that returns some JSX.
+const person = () => {
+
+  return (
+    <p>I'm a Person!</p>
+  )
+
+}
+
+export default person; // <<< This needs to be lowercase because it matches the function name.
